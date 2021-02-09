5a3157704c4e99bcef04a082e2469e122d7a6b3d
AshishVerma18 <aashish.640@gmail.com>
2021-02-09 15:32:30 +0530

    constructor

---
+++ ES6/Constructor.js
@@ -0,0 +1,13 @@
+class student{
+    constructor(Name,Age,Section,Result)
+    {
+        this.name=Name;
+        this.age=Age;
+        this.Section=Section;
+        this.result=Result;
+    
+    }
+}
+
+let stud1= new student("Rohan",21,"H","Pass");
+console.log(stud1);
