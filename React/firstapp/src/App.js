91b5555e9a2537355e9bff170e77438cf4b8609d
AshishVerma18 <aashish.640@gmail.com>
2021-02-22 23:25:33 +0530

    card

--- React/firstapp/src/App.js
+++ React/firstapp/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import Counter from './State/Form/Counter/Counter';
+
 // import Navbar from'./Navbar/Navbar';
-import Form from './State/Form/Form' ;
-import Employee1 from './State/Employee1';
+// import Form from './State/Form/Form' ;
+// import Employee1 from './State/Employee1';
 function App (){
   return(
     <>
-    <Form/>
+    <Counter/>
+    <Counter/>
+    
     </>
   );
 }
