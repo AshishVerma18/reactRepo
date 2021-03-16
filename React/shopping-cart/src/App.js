d35558eac966c4515a8929e2d30f25ba2b538681
AshishVerma18 <aashish.640@gmail.com>
2021-03-16 22:52:20 +0530

    ProductComp

--- React/shopping-cart/src/App.js
+++ React/shopping-cart/src/App.js
@@ -1,15 +1,34 @@
 //feature-1
-import React from 'react';
+import React, { Component } from 'react';
+import Products from './Components/Products';
 
-function App() {
-  return (
+import data from './data.json';
+
+class  App extends Component {
+  constructor(){
+    super();
+    this.state={
+      products:data.products,
+      size:"",
+      sort:"",
+    };
+  }
+  render(){
+    return (
     <>
     <div className="grid-container">
       <header>
         <a href="/">Shopping Cart</a>
       </header>
       <main>
-        Product List
+       <div className="content">
+         <div className="main">
+           <Products products={this.state.products}/>
+         </div>
+         <div className="sidebar">
+           cart items
+         </div>
+       </div>
       </main>
       <footer>
         All rights reserved
@@ -19,6 +38,7 @@ function App() {
     
     
   );
+}
 }
 
 export default App;
