586e102f4c6e3a617ce7a429adb7192eb30db52a
Ashish Verma <77264222+AshishVerma18@users.noreply.github.com>
2024-02-14 11:47:35 +0530

    missingNumber.js

--- JSInterview/matrix.js
+++ JSInterview/matrix.js
@@ -1,1 +1,10 @@
+let missingNumbers = (a, l=true) => Array.from(Array(Math.max(...a)).keys()).map((n, i) => a.indexOf(i) < 0  && (!l || i > Math.min(...a)) ? i : null).filter(f=>f);
 
+var a = [1,4,7], count = a[a.length - 1];
+var missing = [];
+for ( var i = 1; i <= count; i++ ) {
+	if (a.indexOf(i) == -1) {
+		missing.push(i);
+	}
+}
+alert(missing.toString());
