07cf678dee86549dce360a7f263e0adef9bd68a5
Ashish Verma <77264222+AshishVerma18@users.noreply.github.com>
2024-02-14 11:42:48 +0530

    Update reverse.js

--- JSInterview/reverse.js
+++ JSInterview/reverse.js
@@ -1,1 +1,15 @@
+const reverse = (array) => array.map(array.pop, [... array]);
+
+function reverse(array) {
+  var output = [];
+  while (array.length) {
+    output.push(array.pop());
+  }
+
+  return output;
+}
+
+console.log(reverse([1, 2, 3, 4, 5, 6, 7]));
+
+
 
